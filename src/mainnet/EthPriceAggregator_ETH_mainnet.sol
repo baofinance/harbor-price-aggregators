e550cd9498cf5e5cd794cec36de758a31e47075a
rootminus0x1 <rootminus0x1@gmail.com>
2026-04-21 17:27:22 +0100

    fix solhint and veryfy-audit issues

--- src/mainnet/EthPriceAggregator_ETH_mainnet.sol
+++ src/mainnet/EthPriceAggregator_ETH_mainnet.sol
@@ -6,7 +6,5 @@ import {Aggregator_ETH_ETH} from "@harbor-price/aggregators/mainnet/Aggregator_E
 /// @notice Ethereum mainnet ETH/ETH oracle (trivial constant; no feed wiring needed).
 /// @dev Hard-coded wiring for mainnet; deploy scripts select this bytecode by chain.
 /// @custom:oz-upgrades-unsafe-allow constructor
-// solhint-disable-next-line contract-name-capwords
-contract EthPriceAggregator_ETH_mainnet is Aggregator_ETH_ETH {
-    constructor() {}
-}
+// solhint-disable-next-line contract-name-capwords,no-empty-blocks
+contract EthPriceAggregator_ETH_mainnet is Aggregator_ETH_ETH {}
